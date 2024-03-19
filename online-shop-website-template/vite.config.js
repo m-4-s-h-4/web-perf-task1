081fc36cef60c454e8c42f9be1c198440f44be0e
Maria <maria123321borovikova@gmail.com>
2024-03-19 18:46:28 +0100

    main

--- online-shop-website-template/vite.config.js
+++ online-shop-website-template/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import viteImagemin from 'vite-plugin-imagemin';
 import { terser } from 'rollup-plugin-terser';
-
+import { createHtmlPlugin } from 'vite-plugin-html';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
@@ -13,27 +14,32 @@ export default defineConfig({
         optimizationLevel: 7,
       },
       svgo: {
-        plugins: [
-          {
-            removeViewBox: false,
-          },
-        ],
+        plugins: [{ removeViewBox: false }],
       },
     }),
     terser({
-        compress: {
-          drop_console: true,
-        },
-      }),
-      createHtmlPlugin({
-        minify: true, 
-      }),
-      createHtmlPlugin({
-        minify: {
-          collapseWhitespace: true,
-          removeComments: true,
-          removeRedundantAttributes: true,
-        },
-      }),
-    ],
-  });
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        unused: true,
+        dead_code: true,
+      },
+      mangle: {
+        toplevel: true,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+    createHtmlPlugin({
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+      },
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress',
+    }),
+  ],
+});
